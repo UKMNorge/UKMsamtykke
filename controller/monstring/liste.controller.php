a625495114b0209ff2c5278f599a534d0f2eb47e
Marius Mandal <mariusmandal@gmail.com>
2019-01-02 11:32:26 +0100

    Bruk nytt API
    (Men dette er allerede brutt igjen)

--- controller/monstring/liste.controller.php
+++ controller/monstring/liste.controller.php
@@ -3,28 +3,10 @@
 $monstring = new monstring_v2( get_option('pl_id') );
 $TWIGdata['monstring'] = $monstring;
 
-$u13 = new stdClass();
-$u13->id 			= 'u13';
-$u13->navn 			= 'Under 13 år';
-$u13->beskrivelse 	= 'Foresatte må ha bekreftet informasjonen';
-$u13->personer 		= []; 
-
-$u15 = new stdClass();
-$u15->id			= 'u15';
-$u15->navn			= 'Under 15 år';
-$u15->beskrivelse 	= 'Foresatte må ha sett informasjonen';
-$u15->personer 		= [];
-
-$o15 = new stdClass();
-$o15->id			= '15o';
-$o15->navn			= '15 år eller eldre';
-$o15->beskrivelse 	= 'Deltakeren bør ha sett informasjonen';
-$o15->personer[]	= [];
-
 $grupper = [
-	'u13' => $u13,
-	'u15' => $u15,
-	'15o' => $o15
+	'u13' => Kategorier::getById('u13'),
+	'u15' => Kategorier::getById('u15'),
+	'15o' => Kategorier::getById('o15')
 ];
 
 foreach( $monstring->getInnslag()->getAll() as $innslag ) {
--- controller/monstring/sendtoall.controller.php
+++ controller/monstring/sendtoall.controller.php
@@ -7,13 +7,9 @@ $personer = [];
 foreach( $monstring->getInnslag()->getAll() as $innslag ) {
 	foreach( $innslag->getPersoner()->getAll() as $person ) {
 		$samtykke = new samtykke_person( $person, $monstring->getSesong() );
-		
-		if( $samtykke->getStatus()->getId() == 'ikke_sendt' ) {
-			$samtykke->setAttr('melding', samtykke_sms::send('samtykke', $samtykke ));
-		} else {
-			$melding = $samtykke->getKommunikasjon()->get('samtykke');
-			$samtykke->setAttr('melding', 'Samtykke-sms allerede sendt ('. $melding->getTimestamp() .')');
-		}
+    
+        $samtykke->setAttr('melding', $samtykke->getKommunikasjon()->sendMelding('samtykke'));
+        
 		$personer[] = $samtykke;
 	}
 }
