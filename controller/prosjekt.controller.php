e7aac89b6427b62a9317130b34810dbda965c0f6
Marius Mandal <mariusmandal@gmail.com>
2019-10-22 12:11:41 +0200

    Bruk autoloader

--- controller/prosjekt.controller.php
+++ controller/prosjekt.controller.php
@@ -1,21 +1,24 @@
 <?php
 
-require_once('UKM/samtykke/write.class.php');
-require_once('UKM/samtykke/prosjekt.class.php');
+use UKMNorge\Database\SQL\Query;
+use UKMNorge\Samtykke\Prosjekt;
+use UKMNorge\Samtykke\Write;
+
+require_once('UKM/Autoloader.php');
 
 if( $_SERVER['REQUEST_METHOD'] == 'POST' && $_GET['save'] == 'prosjekt' ) {
 	if( $_GET['prosjekt'] == 'new' ) {
-		$prosjekt = write_samtykke::createProsjekt( $_POST['tittel'] );
+		$prosjekt = Write::createProsjekt( $_POST['tittel'] );
 		$_GET['prosjekt'] = $prosjekt->getId();
 	} else {
-		$prosjekt = new samtykke_prosjekt( $_POST['id'] );
+		$prosjekt = new Prosjekt( $_POST['id'] );
 	}
 	$prosjekt->setTittel( $_POST['tittel'] );
 	$prosjekt->setSetning( $_POST['setning'] );
 	$prosjekt->setVarighet( $_POST['varighet'] );
 	$prosjekt->setBeskrivelse( $_POST['beskrivelse'] );
 	
-	write_samtykke::saveProsjekt( $prosjekt );
+	Write::saveProsjekt( $prosjekt );
 
 }
 
@@ -24,10 +27,10 @@ if( isset( $_GET['prosjekt'] ) ) {
 	$TWIGdata['id'] = $_GET['prosjekt'];
 	
 	if( is_numeric( $_GET['prosjekt'] ) ) {
-		$TWIGdata['prosjekt'] = new samtykke_prosjekt( $_GET['prosjekt'] );
+		$TWIGdata['prosjekt'] = new Prosjekt( $_GET['prosjekt'] );
 	}
 } else {
-	$sql = new SQL("
+	$sql = new Query("
 		SELECT * 
 		FROM `samtykke_prosjekt`
 		ORDER BY `tittel` ASC"
@@ -35,8 +38,8 @@ if( isset( $_GET['prosjekt'] ) ) {
 	$res = $sql->run();
 	
 	$prosjekter = [];
-	while( $row = SQL::fetch( $res ) ) {
-		$prosjekter[] = new samtykke_prosjekt( $row );
+	while( $row = Query::fetch( $res ) ) {
+		$prosjekter[] = new Prosjekt( $row );
 	}
 	
 	$TWIGdata['prosjekter'] = $prosjekter;
